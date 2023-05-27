94ab9b873e2a199bf63f226de808df15d1261740
vishnuteertha2001 <134622345+vishnuteertha2001@users.noreply.github.com>
2023-05-27 19:06:47 +0530

    Create sum.c
    This program will scan two numbers and print the sum

---
+++ sum.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main(){
+int n1,n2,sum;
+printf("Enter the first number:");
+scanf("%d",&n1);
+printf("Enter the second number:");
+scanf("%d",&n2);
+sum=n1+n2;
+printf("sum of %d and %d is %d",n1,n2,sum);
+}
